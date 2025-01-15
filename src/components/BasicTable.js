2ce0df012467e7781f3a9e3ae3d645b5e261e6ad
Bobby Steckline <rsteckline@gmail.com>
2025-01-15 14:56:09 -0500

    Create tableInstance to render necessary UI

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -13,22 +13,65 @@ export const BasicTable = () => {
         data: data,
 
     })
+
+    const {
+        /* these are basically functions and arrays that the useTable hooks has given us to enable easy table creation. we need to use all of these w HTML for react table to work as intended. */
+        getTableProps, /* fxn needed to destructure the table tag*/
+        getTableBodyProps, /* fxn needed to destructure the tbody tags */
+        headerGroups, /* contains column heading info that belongs inside the thead tag of table. it's an array that requires map method to render the jsx for each header group, similar to how you would render list in any other components*/
+        rows,
+        prepareRow,
+    } = tableInstance
+
     return (
-        <table>
-            <thead> /* table header */
-                <tr> /* tr tag to specify row  */
-                    <th> /* in the header, the data is wrapped with the th tag */
-
-                    </th>
-                </tr>
+        <table {...getTableProps()}>
+            <thead>
+                {
+                    headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {
+                                headerGroup.headers.map(column => (
+                                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                ))}
+                        </tr>
+                    ))}
             </thead>
-            <tbody> /* table body */
-                <tr> /* tr tag to specify row  */
-                    <td> /* in the body, the data is wrapped with the td tag  */
-
-                    </td>
-                </tr>
+            <tbody  {...getTableBodyProps()}>
+                {
+                    rows.map((row) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })}
+                            </tr>
+                        )
+                    })
+                }
             </tbody>
         </table>
     )
 }
+
+
+
+// return (
+//     <table {...getTableProps()}>
+//         <thead> /* table header */
+//             <tr> /* tr tag to specify row  */
+//                 <th> /* in the header, the data is wrapped with the th tag */
+
+//                 </th>
+//             </tr>
+//         </thead>
+//         <tbody  {...getTableBodyProps()}> /* table body */
+//             <tr> /* tr tag to specify row  */
+//                 <td> /* in the body, the data is wrapped with the td tag  */
+
+//                 </td>
+//             </tr>
+//         </tbody>
+//     </table>
+// )
+// }
